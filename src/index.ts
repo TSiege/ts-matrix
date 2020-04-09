7f2d43b395f9beda79322853f8fa93695c3278f6
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-04-09 11:31:49 -0400

    gets matrix rendering

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+import { NOTES } from './audio'
+import { Matrix } from './components/matrix'
 
+function onReady(fn) {
+  if (document.readyState !== 'loading') {
+    fn()
+  } else {
+    document.addEventListener('DOMContentLoaded', () => fn())
+  }
+}
+
+function renderJsMatrix () {
+  const matrix = new Matrix(NOTES)
+  matrix.renderMatrix()
+}
+
+onReady(renderJsMatrix)
