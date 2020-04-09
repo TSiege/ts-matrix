8200f0e9fa69a5ec4be8342fb21567decb38cbd1
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-04-08 22:20:32 -0400

    moves over to vanilla ts

--- src/components/controller/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import Controller from '.'
-
-test('renders itself', () => {
-  const { getByText } = render(<Controller />)
-  const h1Element = getByText(/JS-Matrix/i)
-  expect(h1Element).toBeInTheDocument()
-})
